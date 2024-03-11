379e303a3b619dab1c7495ec0f84e5583c2be4cd
The Last Cookie <74693705+TheLastCookie-404@users.noreply.github.com>
2024-03-11 16:48:27 +0600

    ItsAlive

---
+++ Game.cs
@@ -0,0 +1,63 @@
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+class Game : GameWindow
+{
+    float r,g,b;
+    public Game(int width, int height, string title) : base(GameWindowSettings.Default, new NativeWindowSettings() { Size = (width, height), Title = title }) 
+    { 
+        Console.WriteLine();
+    }
+
+    protected override void OnUpdateFrame(FrameEventArgs e)
+    {
+        base.OnUpdateFrame(e);
+
+        if(KeyboardState.IsKeyDown(Keys.R)) 
+        {
+            r = 1f;
+            g = 0f;
+            b = 0f;
+        }
+        if(KeyboardState.IsKeyDown(Keys.G)) 
+        {
+            r = 0f;
+            g = 1f;
+            b = 0f;
+        }
+        if(KeyboardState.IsKeyDown(Keys.B)) 
+        {
+            r = 0f;
+            g = 0f;
+            b = 1f;
+        }
+
+        if (KeyboardState.IsKeyDown(Keys.Escape))
+        {
+            Close();
+        }
+    }
+    protected override void OnLoad()
+    {
+        base.OnLoad();
+
+        GL.ClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+        //Code goes here
+    }
+
+    protected override void OnRenderFrame(FrameEventArgs e)
+    {
+        base.OnRenderFrame(e);
+
+        GL.Clear(ClearBufferMask.ColorBufferBit);
+
+        GL.ClearColor(r, g, b, 1.0f);
+
+        //Code goes here.
+
+        SwapBuffers();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿using OpenTK;
-Console.WriteLine("Hello, World!");
+﻿using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+
+GameWindowSettings gameWindowSettings = new();
+NativeWindowSettings nativeWindowSettings = new();
+Game game = new(500, 500, "Shader ESC-close");
+
+
+game.Run();
+
+
+
